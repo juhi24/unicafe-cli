0d5bf34a769730c76d496598ae821256bc23f45f
Aaro Lehikoinen <aaro.lehikoinen@cs.helsinki.fi>
2015-03-31 12:02:51 +0300

    shows also opening hours and exceptions

--- unicafe.py
+++ unicafe.py
@@ -3,12 +3,14 @@
 import urllib2
 import json
 import sys
+import codecs
 import datetime
 import argparse
 
 APIURL ="http://messi.hyyravintolat.fi/publicapi" 
 allprice = ["Edullisesti", "Maukkaasti", "Makeasti", "Kevyesti"]
 today = datetime.date.today()
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 def apidate2date(apidate):
     d=datetime.datetime.strptime(apidate.split(' ')[1], "%d.%m").date()
@@ -18,13 +20,38 @@ def apidate2date(apidate):
 def thisweek(date):
     return today.isocalendar()[1] == date.isocalendar()[1]
 
-def getfood(restaurant, prices):
-    fooddata = json.load(urllib2.urlopen(APIURL + "/restaurant/" + str(restaurant["id"])))
+def daterangestr(date1, date2):
+    return date1 if date1 == date2 else date1 + " - " + date2
 
-    print restaurant["name"]
-    print '='*len(restaurant["name"])
-    print
+def gethours(fooddata):
+    opentime = None
+    closetime = None
+    daystrs = []
+    for time in fooddata["information"]["lounas"]["regular"]:
+        opentime = time["open"]
+        closetime = time["close"]
+        days = []
+        for day in time["when"]:
+            if day and not day == "previous":
+                days.append(day)
+            elif days:
+                daystrs.append([", ".join(days), opentime+"-"+closetime])
+                days=[]
 
+    for dayset,hours in daystrs:
+        print dayset + ": " + hours
+
+    extimes = []
+    for extime in fooddata["information"]["lounas"]["exception"]:
+        if extime["from"] and extime["to"]:
+            days = daterangestr(extime["from"], extime["to"])
+            status = "Suljettu" if extime["closed"] else extime["open"] + "-" + extime["close"]
+            extimes.append(days + ": " + status)
+    if extimes:
+        print "\033[31mPoikkeukset\033[0m"
+        print ", ".join(extimes)
+
+def getfood(fooddata, prices):
     for fd in fooddata["data"]:
         if not fd["data"]:
             continue
@@ -41,9 +68,7 @@ def getfood(restaurant, prices):
             price = f["price"]["name"]
             print "  " + f["name"] + ("" if price=="Edullisesti" else " (" + price + ")")
 
-    print
-
-def main(restaurants, prices):
+def main(restaurants, prices, hours):
     allrestaurants = json.load(urllib2.urlopen(APIURL+"/restaurants"))["data"]
     restaurants = filter(lambda r: r["name"] in restaurants, allrestaurants)
 
@@ -52,15 +77,23 @@ def main(restaurants, prices):
         exit(2)
 
     for restaurant in restaurants:
-        getfood(restaurant, prices)
+        fooddata = json.load(urllib2.urlopen(APIURL + "/restaurant/" + str(restaurant["id"])))
+        print restaurant["name"]
+        print '='*len(restaurant["name"])
+        if hours:
+            gethours(fooddata)
+        print
+        getfood(fooddata, prices)
+        print
 
 parser = argparse.ArgumentParser(description="Get Unicafe lunch lists")
 parser.add_argument("restaurant", nargs="?")
 parser.add_argument("-r", metavar="restaurant", nargs="+", action="append")
 parser.add_argument("-p", metavar="prices", nargs="*", action="append", choices=allprice)
+parser.add_argument("-o", action="count")
 p = None
 
 args = parser.parse_args()
 r = [args.restaurant] if args.restaurant else map(lambda r: r[0], args.r)
 p = allprice if not args.p else map(lambda p: p[0], args.p)
-main(r, p)
+main(r, p, args.o)
